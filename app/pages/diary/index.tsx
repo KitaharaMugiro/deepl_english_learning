a4d81125a2a55b158d2c9bf24a3dff5855bcff31
kotakamikawa <kamikawat8@gmail.com>
2022-08-19 00:03:19 +0900

    サイドメニューに表示

---
+++ app/pages/diary/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Typography } from "@mui/material"
+import Seo from "../../components/common/Seo"
+import MyNoteList from "../../components/mynote/MyNoteList"
+import PhraseList from "../../components/phrase/PhraseList"
+
+export default () => <Container maxWidth="md">
+    <Seo ogpInfo={{
+        title: "英語日記",
+        description: "英語日記を書くことができます。"
+    }} />
+    <Typography component="h1" variant="h3" style={{ marginTop: 40 }}>
+        <b>英語日記</b>
+    </Typography>
+    <Typography variant="subtitle2" component="h2">
+        英語日記を書くことができます。
+    </Typography>
+    <div style={{ height: 15 }} />
+    <MyNoteList />
+</Container>
