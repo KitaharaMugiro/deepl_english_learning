7d6a89e3c5423363763663f5b7ce86e51d0fe460
KitaharaMugiro <you@example.com>
2022-09-16 17:21:12 +0900

    一旦UI完了

--- app/models/util-hooks/useToken.ts
+++ app/models/util-hooks/useToken.ts
@@ -4,18 +4,19 @@ import { TokenApi } from "../../api/TokenApi"
 
 export default () => {
     const [token, setToken] = useState(0)
-    const [tokenRate, setTokenRate] = useState(0)
+    const [tokenRateYen, setTokenRateYen] = useState(0)
+    const [tokenRateMatic, setTokenRateMatic] = useState(0)
     const [createdAt, setCreatedAt] = useState(0)
     useEffect(() => {
         const getToken = async () => {
             const res = await TokenApi.getToken()
             setToken(res.token)
-            setTokenRate(res.tokenRate)
+            setTokenRateYen(res.tokenRate)
             setCreatedAt(res.createdAt)
         }
 
         getToken()
     }, [])
 
-    return { token, tokenRate, createdAt }
+    return { token, tokenRateYen, tokenRateMatic, createdAt }
 }
