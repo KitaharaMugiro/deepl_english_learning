fdb42df43b48370b578e0f5e47a921a8146540e5
KitaharaMugiro <you@example.com>
2022-10-18 11:24:29 +0900

    nftへのリンクを追加

--- app/next.config.original.1666059864088.js
+++ app/next.config.original.1666059864088.js
@@ -1,5 +1,5 @@
 module.exports = function(...args) {
-  let original = require('./next.config.original.1666059864088.js');
+  let original = require('./next.config.original.1663590991858.js');
   const finalConfig = {};
   const target = { target: 'serverless' };
   if (typeof original === 'function' && original.constructor.name === 'AsyncFunction') {
